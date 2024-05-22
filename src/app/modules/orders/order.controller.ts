8d569052fbc4ff933dc5ca2085620fbc82e59ef2
kazirauf <raufelahi9876@gmail.com>
2024-05-22 22:41:26 +0600

    comment add in order.controller file

--- src/app/modules/orders/order.controller.ts
+++ src/app/modules/orders/order.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { OrderValidation } from "./order.validation";
 import { OrderService } from "./order.service";
 
-
+// orderProduct
 export const orderProduct = async (req: Request, res: Response) => {
   try {
     const product = req.body;
@@ -21,7 +21,7 @@ export const orderProduct = async (req: Request, res: Response) => {
     });
   }
 };
-
+// getOrderEmail
 
 export const getOrderEmail = async (req: Request, res: Response) => {
   try {
